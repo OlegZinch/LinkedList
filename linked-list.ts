09e06a7a27129ad8d388ff5c477e3764caf35e60
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-05-28 18:24:22 +0100

    added insertAt and removeAt

--- linked-list.ts
+++ linked-list.ts
@@ -21,6 +21,53 @@ class LinkedList<T> {
     this.length++
   }
 
+  insertAt(value: T, pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root
+      let index = 0
+      let previous = current
+      let node = new ListNode(value)
+
+      if (pos === 0) {
+        node.next = this.root
+        this.root = node
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current
+          current = current.next
+        }
+        node.next = current
+        previous.next = node
+      }
+      this.length++
+      return true
+    } else {
+      return false
+    }
+  }
+
+  removeAt(pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root
+      let previous: ListNode<T> = current
+      let index = 0
+
+      if (pos === 0) {
+        this.root = current.next
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current
+          current = current.next
+        }
+        previous.next = current.next
+      }
+      this.length--
+      return current
+    } else {
+      return null
+    }
+  }
+
   getNumberOfElements() {
     return this.length
   }
